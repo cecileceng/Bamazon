ca4cf2aaf98e09f4b67256f71c7549bb6e0fea48
Cecile Nguyen <cecile.ceng@gmail.com>
2016-07-16 15:09:26 -0500

    inputs working ish

--- bamazon.js
+++ bamazon.js
@@ -30,40 +30,34 @@ function printStuff(res) {
 var startBuying = function() {
 	connection.query('SELECT * FROM products', function(err, res) {
 		printStuff(res);
-		inquirer.prompt({
-			name: 'choice',
-			type: 'rawlist',
-			choices: function(value) {
-				var choiceArray = [];
-				for (var i = 0; i < res.length; i++) {
-					choiceArray.push(res[i].item_name);
-				}
-				return choiceArray;
-			},
-			message: 'Which item would you like to purchase?'
-		}).then(function(answer) {
-			for (var i = 0; i < res.length; i++) {
-				if (res[i].item_name == answer.choice) {
-					var chosenItem = res[i];
-					inquirer.prompt({
-						name: 'bid',
-						type: 'input',
-						message: 'How many would you like to purchase?'
-					}).then(function(answer) {
-						if (chosenItem.highestbid < parseInt(answer.bid)) {
-							connection.query('UPDATE products SET ? WHERE ?', [{ // TODO: Subtract from existing quantity
-								user_purchase: answer.purchase
-							}, {
-								id: chosenItem.id
-							}], function(err, res) {
-								console.log('Your purchase was successful!');
-								startBuying();
-							}); 
-						} else {
-							console.log('There are not enough in stock for you to purchase that many.');
-						}
-					})
-				}
+		var choiceArray = [];
+		for (var i = 0; i < res.length; i++) {
+			choiceArray.push(res[i].product_name);
+		}
+		inquirer.prompt([{
+			name: 'purchase',
+			type: 'input',
+			message: 'Which item would you like to purchase? (Enter the Item ID)'
+		},
+		{
+			name: 'quantity',
+			type: 'input',
+			message: 'How many would you like to purchase?'
+		}]).then(function(answer) {
+			console.log(itemID);
+			var chosenItem = res[itemID-1];
+			console.log(chosenItem);
+			if (chosenItem.highestbid < parseInt(answer.bid)) {
+				connection.query('UPDATE products SET ? WHERE ?', [{ // TODO: Subtract from existing quantity
+					user_purchase: answer.purchase
+				}, {
+					id: chosenItem.id
+				}], function(err, res) {
+					console.log('Your purchase was successful!');
+					startBuying();
+				}); 
+			} else {
+				console.log('There are not enough in stock for you to purchase that many.');
 			}
 		})
 	})
